7480895fc7c9ba40df99b9bcc44b534b67842e76
ormoyo <ormoyoo@proton.me>
2024-08-10 20:29:25 +0300

    Implemented anki docker service

---
+++ modules/docker-containers/anki.nix
@@ -0,0 +1,22 @@
+{ name, path, id, getSecret, ... }:
+{
+  project.name = name;
+  host.uid = id;
+  services = {
+    app.service = {
+      container_name = name;
+      image = "zweizs/anki-sync-server";
+      restart = "unless-stopped";
+      networks = [ "frontend" ];
+      volumes = [ 
+        "${path}/data:/data" 
+      ];
+      env_file = [ (getSecret "users") ];
+    };
+  };
+  enableDefaultNetwork = false;
+  networks.frontend = {
+    name = "main-nginx";
+    external = true;
+  };
+}
