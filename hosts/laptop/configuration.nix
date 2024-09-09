9fac9c81a17ed30755a00a6836fe29c5f13dd6c9
Ormoyo <ormoyoo@proton.me>
2024-09-09 17:35:33 +0300

    Updated laptop u2f config

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -72,17 +72,27 @@
   hardware.bluetooth.enable = true;
   services.xserver.libinput.enable = true;
 
-  services.udev.packages = [ pkgs.yubikey-personalization ];
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
   };
 
-  security.pam.u2f.authFile = "/etc/u2f_keys";
-  security.pam.services = {
-    login.u2fAuth = true;
-    sudo.u2fAuth = true;
-  };
+  services.udev.packages = [ pkgs.yubikey-personalization libu2f-host ];
+  security.pam = {
+    services = {
+      login.u2fAuth = true;
+      sudo.u2fAuth = true;
+      gdm-password.u2fAuth = true;
+    };
+    u2f = {
+      enable = true;
+      cue = true;
+      origin = "pc.org";
+      appId = "pc.org";
+      authFile = pkgs.writeText "u2f_keys"
+        ''
+          ormoyo:VcS4hSXGlWEzsioA4BxgWoHDVGYn2rFdxyPJGYEmPvo+pajdUEdhHJPWlr7B4e3VhNAmpNMXKc/9SSJP6h+MCg==,SSmCTMgYrqorzGg5w0Pi6gAEx48aKoP63BP/TmyxRdzWX0SND0zNYZbFPw7ErhwFdYXaDcZAL5Vlsc86zwq+Lg==,es256,+presence:J8Zz4HGOhbUMdsfRkLNLBFrx30J1j8nN6um8B1ZPjGefc/jRVvhDH85c2RnUHP9K/5N2G3n5ETSgTvsgipcljg==,7rRjwIt/Ra65pHtAbKf+VzUdJu+MV7z9dVxVdg6A5UOAWa3spfmQDXiNwISKZt90xZ4plrbE/SEJTugj9jD0lw==,es256,+presence
+        '';
     };
   };
 
