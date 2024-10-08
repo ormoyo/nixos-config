66c2e3d0e7d325ff2e57ab9a7d65b4ccb6161337
Ormoyo <ormoyoo@proton.me>
2024-10-08 21:18:32 +0300

    Implemented minecraft server

---
+++ modules/nixos/docker/minecraft.nix
@@ -0,0 +1,27 @@
+{ name, path, id, getSecret, ... }:
+{
+  project.name = name;
+  host.uid = id;
+  services = {
+    app.service = {
+      container_name = name;
+      image = "itzg/minecraft-server";
+      restart = "unless-stopped";
+      networks = [ "frontend" ];
+      ports = [ "25565:25565" ];
+      volumes = [
+        "${path}/data:/data"
+      ];
+      environment = {
+        EULE = "true";
+        MAX_MEMORY = "4G";
+        TYPE = "VANILLA";
+      };
+    };
+  };
+  enableDefaultNetwork = false;
+  networks.frontend = {
+    name = "main-nginx";
+    external = true;
+  };
+}
