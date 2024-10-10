0895348d6c70228ab82cff15372f1c5f9eec8016
ormoyo <ormoyoo@proton.me>
2024-10-10 14:32:01 +0300

    Fixed importing common module without "config" arg

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }@attrs:
 {
   imports = [
-    (import ./common { inherit (attrs) inputs lib; inherit pkgs; cfg = config.settings.common; })
+    (import ./common { inherit (attrs) inputs lib; inherit pkgs config; cfg = config.settings.common; })
     ./backups.nix
     ./docker
   ];
