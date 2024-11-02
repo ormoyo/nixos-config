731d85abe0f016ae8c14f33005b9227ad7178642
Ormoyo <ormoyoo@proton.me>
2024-11-02 19:04:48 +0200

    Created a vpn service for other docker containers

---
+++ modules/nixos/docker/vpn.nix
@@ -0,0 +1,25 @@
+{ name, path, id, getSecret, ... }: 
+{
+  project.name = name;
+  host.uid = id;
+  custom.secrets = [ "config" ];
+  services = {
+    app.service = {
+      container_name = name;
+      image = "ghcr.io/qdm12/gluetun";
+      restart = "unless-stopped";
+      networks = [ "frontend" ];
+      devices = [ "/dev/net/tun:/dev/net/tun" ];
+      env_file = [ (getSecret "TZ") (getSecret "config") ];
+      capabilities = {
+        ALL = true;
+        NET_ADMIN = true;
+      };
+    };
+  };
+  enableDefaultNetwork = false;
+  networks.frontend = {
+    name = "main-nginx";
+    external = true;
+  };
+}
