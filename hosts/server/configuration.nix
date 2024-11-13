8ac132f6c50706d221980d96392ea18374a21722
Ormoyo <ormoyoo@proton.me>
2024-11-13 22:13:35 +0200

    Added mods dir for furry server

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -33,6 +33,9 @@
       furryIL = {
         autoStart = true;
         package = pkgs.forgeServers.forge-1_20_1;
+        symlinks = {
+          mods = ./servers/furryIL/mods;
+        };
         serverProperties = {
           server-port = 43000;
           difficulty = 3;
