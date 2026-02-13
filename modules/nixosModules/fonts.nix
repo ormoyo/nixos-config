4c277b51839d43f74aad7b214cc08d5ad8e54ebb
Ormoyo <ormoyoo@proton.me>
2026-02-13 18:38:02 +0200

    Add fonts module

---
+++ modules/nixosModules/fonts.nix
@@ -0,0 +1,27 @@
+{ inputs, ... }:
+{
+  flake.nixosModules.fonts = { pkgs, ... }: {
+    environment.systemPackages = [ pkgs.fcitx5 ];
+
+    # Enable IME
+    i18n.inputMethod = {
+      type = "fcitx5";
+      fcitx5 = {
+        waylandFrontend = true;
+        addons = with pkgs; [
+          fcitx5-mozc
+          fcitx5-gtk
+        ];
+      };
+    };
+
+    fonts.packages = with pkgs; [
+      corefonts
+      noto-fonts-cjk-sans
+      noto-fonts-emoji
+      noto-fonts-extra
+      nerd-fonts.hurmit
+      ipafont
+    ];
+  };
+}
