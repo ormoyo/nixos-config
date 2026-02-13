d95b39a01bd156da5e3663ec0211136efd276eeb
Ormoyo <ormoyoo@proton.me>
2026-02-13 18:38:16 +0200

    Add virtualisation module

---
+++ modules/nixosModules/virtualisation.nix
@@ -0,0 +1,22 @@
+{ inputs, ... }:
+{
+  flake.nixosModules.virtualisation = { pkgs, ... }: {
+    virtualisation.libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        runAsRoot = true;
+        swtpm.enable = true;
+      };
+    };
+
+    virtualisation.podman.enable = true;
+    programs.virt-manager.enable = true;
+
+    users.users.ormoyo = {
+      extraGroups = [ "libvirtd" ];
+    };
+
+    environment.systemPackages = with pkgs; [ virtiofsd ];
+  };
+}
